d4ce8f9e7eda7b54cfe7e4ae1caf24b7a8eb0384
Matheus <mat.feltrin@gmail.com>
2023-11-04 10:37:01 -0300

    feat: upload challange

---
+++ channels.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func ping(c chan string) {
+	for i := 0; ; i++ {
+		c <- "ping"
+	}
+}
+
+func pong(c chan string) {
+	for i := 0; ; i++ {
+		c <- "pong"
+	}
+}
+
+func prints(c chan string) {
+	for {
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func main() {
+	var c chan string = make(chan string)
+
+	go ping(c)
+	go prints(c)
+	go pong(c)
+
+	var entry string
+	fmt.Scanln(&entry)
+}
